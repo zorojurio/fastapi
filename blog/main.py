b9c67d5ff9f6526c70c6fffa85086af9b3f7510e
zorojurio <zorojurio@gmail.com>
2022-03-01 08:46:27 +0530

    delete blog

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 
 from . import models, schemas
@@ -17,7 +17,7 @@ def get_db():
         db.close()
 
 
-@app.post('/blogs/')
+@app.post('/blogs/', status_code=status.HTTP_201_CREATED)
 def create(blog: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=blog.title, body=blog.body)
     db.add(new_blog)
@@ -27,3 +27,25 @@ def create(blog: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+@app.get('/blogs')
+def all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blogs/{blog_id}', status_code=200)
+def blog_detail(blog_id: int, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+    if not blog:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"detail": f"Blog with the {blog_id} is not available"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {blog_id} is not found")
+    return blog
+
+
+@app.delete('/blogs/{blog_id}/delete', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(blog_id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id)
+    blog.delete()
+    db.commit()
+    return "Done"
