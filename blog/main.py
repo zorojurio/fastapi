e0cac8e2a705cda6f77b5767cf57fced45758fc1
zorojurio <zorojurio@gmail.com>
2022-03-01 20:29:07 +0530

    relationships

--- blog/main.py
+++ blog/main.py
@@ -5,10 +5,22 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 from .database import engine, SessionLocal
+from .hashing import Hash
 
 models.Base.metadata.create_all(bind=engine)
 print("creating")
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "blogs",
+        "description": "Manage blogs. So _fancy_ they have their own docs.",
+    },
+]
+
+app = FastAPI(openapi_tags=tags_metadata)
 
 
 def get_db():
@@ -19,9 +31,9 @@ def get_db():
         db.close()
 
 
-@app.post('/blogs', status_code=status.HTTP_201_CREATED)
+@app.post('/blogs', status_code=status.HTTP_201_CREATED, tags=["blogs"])
 def create(blog: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=blog.title, body=blog.body)
+    new_blog = models.Blog(title=blog.title, body=blog.body, user_id=1)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
@@ -29,13 +41,13 @@ def create(blog: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blogs', response_model=List[schemas.ShowBlog])
+@app.get('/blogs', response_model=List[schemas.ShowBlog], tags=["blogs"])
 def all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blogs/{blog_id}', status_code=200, response_model=schemas.ShowBlog)
+@app.get('/blogs/{blog_id}', status_code=200, response_model=schemas.ShowBlog, tags=["blogs"])
 def blog_detail(blog_id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
     if not blog:
@@ -45,7 +57,7 @@ def blog_detail(blog_id: int, db: Session = Depends(get_db)):
     return blog
 
 
-@app.delete('/blogs/{blog_id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blogs/{blog_id}', status_code=status.HTTP_204_NO_CONTENT, tags=["blogs"])
 def delete_blog(blog_id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id)
     if not blog.first():
@@ -55,7 +67,7 @@ def delete_blog(blog_id, db: Session = Depends(get_db)):
     return "Done"
 
 
-@app.put('/blogs/{blog_id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blogs/{blog_id}', status_code=status.HTTP_202_ACCEPTED, tags=["blogs"])
 def update_blog(blog_id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id)
     if not blog.first():
@@ -65,11 +77,26 @@ def update_blog(blog_id, request: schemas.Blog, db: Session = Depends(get_db)):
     return blog.first()
 
 
-@app.post('/user')
+@app.post('/user', response_model=schemas.UserOut, tags=["users"])
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    print(request.dict())
-    user = models.User(username=request.username, password=request.password, email=request.email)
+    user = models.User(username=request.username, password=Hash.bcrypt(request.password), email=request.email)
     db.add(user)
     db.commit()
     db.refresh(user)
     return user
+
+
+@app.post('/users', response_model=List[schemas.UserOut], tags=["users"])
+def list_user(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+
+@app.get('/users/{user_id}', response_model=schemas.UserOut, tags=["users"])
+def user_detail(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {user_id} not found')
+    return user
+
+
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,15 +1,14 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
-class Blog(BaseModel):
+class BaseBlog(BaseModel):
     title: str
     body: str
 
 
-class ShowBlog(BaseModel):
-    title: str
-    body: str
-
+class Blog(BaseBlog):
     class Config:
         orm_mode = True
 
@@ -18,3 +17,21 @@ class User(BaseModel):
     username: str
     password: str
     email: str
+
+
+class UserOut(BaseModel):
+    username: str
+    email: str
+    blogs: List[Blog]
+
+    class Config:
+        orm_mode = True
+
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: UserOut
+
+    class Config:
+        orm_mode = True
